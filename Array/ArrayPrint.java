75b2c82e8a6ed537cf84062545390b77017423ca
Bikash Kumar Giri <giribikashkumar097@gmail.com>
2024-06-27 20:54:15 +0530

    Merge Two Sorted Arrays

--- Array/ArrayPrint.java
+++ Array/ArrayPrint.java
@@ -1,7 +1,7 @@
 package Array;
 
 public class ArrayPrint {
-    public void arrayPrint(int arr[]){
+    public static void arrayPrint(int arr[]){
         for(int i=0; i<arr.length;i++){
             System.out.print(arr[i]+", ");
         }
